9de85160f0338626733d7fe12e78554710f4c7b0
burakintisah <45984196+burakintisah@users.noreply.github.com>
2019-12-31 15:05:45 +0300

    Second Commit

--- simplefs.c
+++ simplefs.c
@@ -47,8 +47,12 @@ typedef struct
     int isUsed ;                 // whether the file descriptor is being used
     int file_next;               // next data block to read
 
-} fd;
+} fileDes;
 
+typedef struct {
+    char name [MAX_FILENAME_LEN]; 
+    int  openedFile;
+} opened;
 
 int vdisk_fd; // global virtual disk file descriptor
               // will be assigned with the sfs_mount call
@@ -56,12 +60,12 @@ int vdisk_fd; // global virtual disk file descriptor
 
 superBlock * ptr_superBlock;
 fileInfo fileInfos [FILE_COUNT_PER_BLOCK];
-fd fat [MAX_FAT_ENTRIES];
-int openFiles [MAX_NUMBER_OPEN_FILE];
+fileDes fat [MAX_FAT_ENTRIES];
+opened openFiles [MAX_NUMBER_OPEN_FILE];
 int openFileCount = 0;
 
 for (int i=0; i<MAX_NUMBER_OPEN_FILE; i++){
-    openFiles[i] = -1;
+    openFiles[i]->openedFile = -1;
 }
 
 int numBlock ; 
@@ -207,27 +211,29 @@ int sfs_create(char *filename)
 }
 
 
-int sfs_open(char *file, int mode)
-{
-    int file_place [2] = find_file(file);
+int sfs_open(char *filename, int mode){
+    int file_place [2] = find_file(filename);
 
     read_block(fileInfos,file_place[0]);
 
     fileInfo * file = fileInfos[file_place[1]];
-
-    if (file->mode == mode || file->num_fd == 0 ){
-        file->mode = mode;
-        file->num_fd += 1;
-    }
 
     if (file->mode != mode && file->num_fd > 0){
         printf("Error: The file is already opened in different mode.\n");
         return (-1);
     }
 
-    write_block(fileInfos,file_place[0]);
+    else if (file->mode == mode || file->num_fd == 0){
+        file->mode = mode;
+        file->num_fd += 1;
+    }
 
-    if (file ->head == -1 ){
+    if (openFileCount == MAX_NUMBER_OPEN_FILE ){
+         printf("Error: Max limit reached for opening a file.\n");
+         return (-1);
+    }
+
+    if (file -> head == -1 ){
         int file_d [2] = find_empty_fat_entry ();
         if(file_d [0] == -1 ){
             printf("Error: No empty fat entry.\n");
@@ -243,17 +249,18 @@ int sfs_open(char *file, int mode)
 
         int desc = (file_d[0] * 128) + file_d[1];
         int empty = -1;
+        file->head = desc;
 
-        for (int i = 0 ; i<openFileCount; i++){
-            if (openFiles[i] == -1 ){
-                openFiles[i] = desc;
-                empty = 0;
+        for (int i = 0 ; i < MAX_NUMBER_OPEN_FILE ; i++){
+            if (openFiles[i]->openedFile == -1 ){
+                openFiles[i]->openedFile = desc;
+                openFiles[i]->name = filename;
+                openFileCount += 1;
+                break;
             }
         }
 
-        if (empty == -1){
-            printf("Error: Max limit reached for opening a file.\n");
-        }
+        write_block(fileInfos,file_place[0]);
 
         return desc;
     }
@@ -267,27 +274,115 @@ int sfs_open(char *file, int mode)
 
 int sfs_close(int fd){
 
-    for (int i = 0 ; i<openFileCount; i++){
-        if (openFiles[i] == fd ){
-            openFiles[i] = -1;
+    for (int i = 0 ; i < MAX_NUMBER_OPEN_FILE; i++){
+        if (openFiles[i]->openedFile == fd ){
+            openFiles[i]->openedFile = -1;
+            openFileCount -= 1;
+            return 0;
         }
     }
-    return (0); 
+    printf("Error: There is no opened file with given descriptor.\n");
+    return (-1); 
 }
 
-int sfs_getsize (int fd)
-{
-
-    return (0); 
+int sfs_getsize (int fd){
+    for (int i = 0 ; i < MAX_NUMBER_OPEN_FILE; i++){
+        if (openFiles[i]->openedFile == fd ){
+            char * file_name = openFiles[i]->name;
+            int* place = find_file(file_name);
+            read_block(fileInfos,place[0]);
+            return fileInfos[place[1]]->size;
+        }
+    }
+    return (-1); 
 }
 
 int sfs_read(int fd, void *buf, int n){
-    return (0); 
+
+    int blockNo = fd / 128;
+    int indexNo = fd % 128;
+
+    read_block(fat,blockNo);
+
+    fileDes found = fat[indexNo];
+
+    char dataBlock [1024];
+    read_block(dataBlock, fd + 1032 );
+
+    // whether data block's size is smaller than n 
+    int dataSize = sfs_getsize(fd);
+    if(dataSize < n){
+        n = dataSize;
+    }
+
+    if(n <= 1024){
+        for (int i=0; i<n;i++){
+            buf[i] = dataBlock[i];
+        }
+        return n;
+    }
+    int count = 0;
+    while (n > 1024){
+
+        for (int i=0; i<1024;i++){
+            buf[count + i] = dataBlock[i]; 
+        }
+        count += 1024;
+        n = n - 1024; 
+        if (found->file_next == -1)
+            break;
+        
+        fd = found->file_next;
+        read_block(dataBlock,fd+1032);
+    }
+
+    for (int i=0; i<n;i++){
+        buf[count + i] = dataBlock[i];
+    }
+    count += n;
+    return count; 
 }
 
 
-int sfs_append(int fd, void *buf, int n)
-{
+int sfs_append(int fd, void *buf, int n){
+
+    int blockNo = fd / 128;
+    int indexNo = fd % 128;
+
+    read_block(fat,blockNo);
+    fileDes found = fat[indexNo];
+    char dataBlock [1024];
+
+    while (found->file_next != -1 ){
+        fd = found->file_next;
+        blockNo = fd / 128;
+        indexNo = fd % 128;
+
+        read_block(fat,blockNo);
+        found = fat[indexNo];
+    }
+
+    read_block(dataBlock, fd + 1032 );
+
+    int boyut = sfs_getsize(fd);
+    int doluluk = boyut % 1024;
+    int bosluk = 1024 - doluluk;
+
+    if (n<bosluk){
+        for (int i= 1; i <= n; i++){
+            dataBlock[doluluk+i] = buf[i-1];
+        }
+    }
+    while (n>bosluk){
+        for (int i= 1; i <= bosluk; i++){
+            dataBlock[doluluk+i] = buf[i-1];
+        }
+        n -= bosluk;
+
+        
+    }
+
+
     return (0); 
 }
 
